d4522eed1a1802be20cec34ac5fda387ce6fe8d8
Lars Wischhof <wischhof@ieee.org>
2019-05-07 11:00:30 +0200

    added first version of vadere container and start script

---
+++ docker/vadere/init.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# 
+echo "Welcome to the roVer vadere Docker Container."
+
+# start vadere GUI
+java -jar /opt/vadere/vadere/VadereGui/target/vadere-gui.jar
+
+echo "Container terminated."
