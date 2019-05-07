386cf888c7a1421cca71be24a271772c4b196993
Lars Wischhof <wischhof@ieee.org>
2019-05-07 23:39:43 +0200

    added upload to registry in (highly redundant...) update_container scripts

--- docker/omnetpp/update_container.sh
+++ docker/omnetpp/update_container.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker build -t rover/omnetpp .
+docker build -t sam-dev.cs.hm.edu:5023/rover/rover-main/omnetpp:latest .
+
+if [ $? -eq 0 ]; then
+   docker login sam-dev.cs.hm.edu:5023
+   docker push sam-dev.cs.hm.edu:5023/rover/rover-main/omnetpp:latest
+else 
+   echo "Container build did not succeed - no upload to registry."
+fi
--- docker/sumo/update_container.sh
+++ docker/sumo/update_container.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-docker build -t rover/sumo .
+docker build -t sam-dev.cs.hm.edu:5023/rover/rover-main/sumo:latest .
+
+if [ $? -eq 0 ]; then
+   docker login sam-dev.cs.hm.edu:5023
+   docker push sam-dev.cs.hm.edu:5023/rover/rover-main/sumo:latest
+else 
+   echo "Container build did not succeed - no upload to registry."
+fi
