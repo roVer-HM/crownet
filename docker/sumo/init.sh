0c04b8d2e4d2a5bbe19e663d31fb1e5a700cbcb3
Lars Wischhof <wischhof@ieee.org>
2019-05-10 11:14:44 +0200

    interaction of omnet and sumo containers

--- docker/sumo/init.sh
+++ docker/sumo/init.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 # 
+PORT=9999
+COMMAND=sumo-gui
+# IP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+
 echo "Welcome to the roVer sumo Docker Container."
+echo ""
+echo "The sumo-launchd is waiting for incoming TraCI connections on port: $PORT"
+echo "($COMMAND will be started automatically for incoming TraCI connections.)"
+echo ""
+echo "If you want to start sumo-gui manually, you can do this via docker exec:"
+echo "  docker exec sumo sumo-gui" 
 
 export PATH=/opt/sumo/sumo/bin:$PATH
 
-# start sumo GUI
-sumo-gui
+# start sumo-launchd (command: sumo-gui, sumo)
+/opt/veins/sumo-launchd.py -v --bind=0.0.0.0 --command=sumo-gui 
 
 echo "Container terminated."
--- scripts/lib/lib.sh
+++ scripts/lib/lib.sh
@@ -5,11 +5,23 @@
 # This bash script contains bash functions/methods used in various other
 # scripts in order to avoid redundant code.
 
+INTNET="rovernet"
+
+#
+function check_create_network() {
+	if [ ! "$(docker network ls | grep $INTNET)" ]; then
+	  echo "Creating $INTNET network ..."
+	  docker network create $INTNET
+	else
+	  echo "Network $INTNET for communication between the roVer containers exists."
+	fi
+}
 
 # Run a docker container allowing X11 output.
 # (For an interactive shell, add the options -ti .)
 # @param $@ parameters to be forwarded to the docker run command
 function run_container_X11() {
+	check_create_network
 	# allow X11 access for the docker user
 	xhost +"local:docker@"
 	# xhost +"local:root"
@@ -20,7 +32,8 @@ function run_container_X11() {
 	docker run -t  \
         --cap-add=SYS_PTRACE \
 	--user $(id -u) \
-	-e DISPLAY=$DISPLAY \
+        --network $INTNET \
+	--env DISPLAY=$DISPLAY \
 	--workdir=$(pwd) \
 	--volume="/home/$USER:/home/$USER" \
 	--volume="/etc/group:/etc/group:ro" \
