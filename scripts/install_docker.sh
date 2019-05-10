8445253f3d8a5cc800ae9ef5102fca8da7584a22
Lars Wischhof <wischhof@ieee.org>
2019-05-10 15:54:14 +0200

    bugfix: add write-access to OMNeT++ ide folder (so that OMNeT++ can write log file)

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -11,8 +11,8 @@ echo "When queried, please enter login credentials for accessing the Docker regi
 echo ""
 docker login sam-dev.cs.hm.edu:5023
 
-echo "Adding $USER to docker group (change requires to login again or do a reboot!)."
+echo "Adding $USER to docker group (change requires a reboot!)."
 sudo usermod -a -G docker $USER
-echo "Done."
+echo "Done. Please reboot now and login again."
 
 
