d9691746f036f4cec0e0a974c3f147f89fb7a4e8
Lars Wischhof <wischhof@ieee.org>
2019-05-10 15:16:13 +0200

    bugfix: added missing Docker login in installation script

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -7,6 +7,10 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update && sudo apt -y install docker-ce
 
+echo "When queried, please enter login credentials for accessing the Docker registry at sam-dev.cs.hm.edu."
+echo ""
+docker login sam-dev.cs.hm.edu:5023
+
 echo "Adding $USER to docker group (change requires to login again or do a reboot!)."
 sudo usermod -a -G docker $USER
 echo "Done."
