cc28c472cd8e5ec29a970b8997839d5352908206
Lars Wischhof <wischhof@ieee.org>
2019-10-10 16:18:50 +0200

    fix: check that install script is not executed as root user

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -2,6 +2,11 @@
 #
 # This script installs docker on a Ubuntu 18.04 machine.
 
+if (( $EUID == 0 )); then
+    echo "This script must NOT be run as a root. Please start it without sudo."
+    exit
+fi
+
 sudo apt -y install apt-transport-https ca-certificates curl
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
