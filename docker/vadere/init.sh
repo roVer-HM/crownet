49544daaa4b1baa9da4b45c261b9c7679caf5587
stsc <stefan.schuhbaeck@hm.edu>
2019-11-27 12:32:28 +0100

    fix sumo and vadere script to start docker in the correct way
    * only wrap 'exec' command in a waiting script . If nothing is provided (expect of container version)
    * remove workaround in vadere container. -> use exec  to start external vadere JAR

--- docker/vadere/init.sh
+++ docker/vadere/init.sh
@@ -1,22 +1,10 @@
 #!/bin/bash
-# 
+#
 echo "Welcome to the roVer vadere Docker Container."
 echo ""
 
 # default jar to be started when container is launched:
 JAR_PATH=/opt/vadere/vadere/VadereGui/target/vadere-gui.jar
-
-if (( "$#" != 0 )) 
-then
-    echo "Starting manually specified jar: $1"
-    if (( "$#" != 1 ))
-    then
-        echo "Arguments: $2 $3 $4 $5 $6 $7 $8 $9"
-    fi
-    JAR_PATH=$1
-else 
-    echo "No manually specified jar - starting default jar: $JAR_PATH"
-fi
 
 if [ ! -f $JAR_PATH ]; then
     echo "Error: jar file $JAR_PATH not found."
