39cce6cb61cf591b7d69f1574d8741fcc2a98b1d
stsc <stefan.schuhbaeck@hm.edu>
2019-11-27 18:10:37 +0100

    revert vadere exec support.
    version selection and exec support not possible for vadere container. The container must be started with external jar files during development. This interferes with the version / exec calls for sumo and omnetpp container

--- docker/vadere/init.sh
+++ docker/vadere/init.sh
@@ -6,6 +6,18 @@ echo ""
 # default jar to be started when container is launched:
 JAR_PATH=/opt/vadere/vadere/VadereGui/target/vadere-gui.jar
 
+if (( "$#" != 0 ))
+then
+    echo "Starting manually specified jar: $1"
+    if (( "$#" != 1 ))
+    then
+        echo "Arguments: $2 $3 $4 $5 $6 $7 $8 $9"
+    fi
+    JAR_PATH=$1
+else
+    echo "No manually specified jar - starting default jar: $JAR_PATH"
+fi
+
 if [ ! -f $JAR_PATH ]; then
     echo "Error: jar file $JAR_PATH not found."
     echo "Hint:  Within the container, only specific paths - e.g. your home directory $HOME - are visible!"
