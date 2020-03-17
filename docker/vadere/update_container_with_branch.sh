90a16ffd2d2293a13c912521d257163036bbe01d
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2020-03-17 19:28:29 +0100

    fix vadere docker creation/handling

--- docker/vadere/update_container_with_branch.sh
+++ docker/vadere/update_container_with_branch.sh
@@ -4,11 +4,16 @@ IMAGE='sam-dev.cs.hm.edu:5023/rover/rover-main/vadere'
 DATE_TAG="$(date "+%y%m%d-%H%M")"
 docker build -t "$IMAGE:latest" -t "$IMAGE:$DATE_TAG" -t "$IMAGE:branch_$1" --build-arg NOCACHE_PULL=$RANDOM --build-arg BRANCH=$1 .
 
-if [ $? -eq 0 ]; then
-   docker login sam-dev.cs.hm.edu:5023
-#   docker push "$IMAGE:latest"
+if [ $? -ne 0 ];then
+   echo "Container build did not succeed - no upload to registry."
+   exit -1
+fi
+
+if [ ! -z $2 ]; then
+   echo "docker login sam-dev.cs.hm.edu:5023"
+   docker push "$IMAGE:latest"
    docker push "$IMAGE:$DATE_TAG"
    docker push "$IMAGE:branch_$1"
 else
-   echo "Container build did not succeed - no upload to registry."
+   echo "OK - no upload to registry."
 fi
