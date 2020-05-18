e34ed0305c3fc7a2756857469cfa471d440df60b
Christina <mayr9@hm.edu>
2020-05-18 16:16:44 +0200

    extended run_script functionality
    Former-commit-id: 26fe9d40f07872f24c945f4ff890b42b2478a3d7

--- rover/simulations/simple_detoure_suqc/run_script.py
+++ rover/simulations/simple_detoure_suqc/run_script.py
@@ -2,7 +2,6 @@
 # import roveranalzer
 # import logging # <-- runSim.py erzeuge logfile runSim.log
 import os
-import shutil
 import subprocess
 import sys
 
@@ -11,139 +10,170 @@ import pandas as pd
 import time
 import glob
 import argparse
+import shutil
+
+class SimulationRun:
+
+    def __init__(self, qoi, remove = False ):
+
+        self.qoi = qoi
+        self._set_old_files()
+        self.remove = remove
+
+    def _set_old_files(self):
+        filepath = f"{os.getcwd()}/results/**/*.scenario"
+        old_files = list()
+        for file in glob.glob(filepath, recursive=True):
+            old_files.append(os.path.dirname(file))
+        self.old_files = old_files
+
+    def _get_old_files(self):
+        return self.old_files
+
+    def run_sim(self):
+        os.system("chmod +x runScript.sh")
+        return_code = subprocess.check_call(
+            ["./runScript.sh"],
+            env=os.environ,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.STDOUT,
+        )
+
+    def _get_current_filepath(self, qoi):
+        filepath = f"{os.getcwd()}/results/**/{qoi}"
+        filepaths = glob.glob(filepath, recursive=True)
+        old_files = self._get_old_files()
+        old_files = [os.path.join(path,qoi) for path in old_files]
+        new_path = [i for i in filepaths if i not in old_files]
+        return new_path
 
 
-def run_sim():
-    os.system("chmod +x runScript.sh")
-    return_code = subprocess.check_call(
-        ["./runScript.sh"],
-        env=os.environ,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.STDOUT,
-    )
+    def wait_for_qoi(self,filename):
+
+        file = self._get_current_filepath(filename)
+
+        secs = 0
+        while (len(file) < 1) and (secs <= 60):
+            file = self._get_current_filepath(filename)
+            time.sleep(1)
+            secs += 1
+
+        if (len(file) < 1):
+            file = None  # prevent from infinite loop
+        else:
+            file = file[0]
+
+        return file
 
 
-def wait_for_qoi(filename):
-    filepath = f"{os.getcwd()}/results/**/{filename}"
-    file = glob.glob(filepath, recursive=True)
-
-    secs = 0
-    while (len(file) < 1) and (secs <= 60):
-        file = glob.glob(filepath, recursive=True)
-        time.sleep(1)
-        secs += 1
-
-    if (len(file) < 1):
-        file = None  # prevent from infinite loop
-    else:
-        file = file[0]
-
-    return file
+    def get_DegreeInformed_Dataframe(self,filepath):
+        df_r = pd.read_csv(filepath, delimiter=" ", header=[0], comment="#")
+        dt = 0.4  # one time frame = 0.4s
+        df_r = df_r.iloc[249:, :]
+        df_r["timeStep"] = (
+                dt * df_r["timeStep"] - 100.0
+        )  # information dissemination starts at 100s
+        df_r.index = df_r.index - 249
+        return df_r
 
 
-def get_DegreeInformed_Dataframe(filepath):
-    df_r = pd.read_csv(filepath, delimiter=" ", header=[0], comment="#")
-    dt = 0.4  # one time frame = 0.4s
-    df_r = df_r.iloc[249:-1, :]
-    df_r["timeStep"] = (
-            dt * df_r["timeStep"] - 100.0
-    )  # information dissemination starts at 100s
-    df_r.index = df_r.index - 249
-    return df_r
+    def DegreeInformed_extract(self):
+        filename = "DegreeInformed.txt"
+        # wait for file
+        filepath = self.wait_for_qoi(filename)
+        # replace it with file extraction
+        df_r = self.get_DegreeInformed_Dataframe(filepath)
+        df_r.to_csv(os.path.join(os.path.dirname(filepath), "DegreeInformed_extract.txt"), sep=" ")
+
+        # plot
+        plt.plot(df_r.iloc[:, 0], df_r.iloc[:, 3])
+        plt.axhline(y=0.95, c="r")
+        plt.xlabel("Time [s] (Time = 0s : start of information dissemination)")
+        plt.ylabel("Percentage of pedestrians informed [%]")
+        plt.title("Information degree")
+        plt.savefig(os.path.join(os.path.dirname(filepath), "InformationDegree.png"))
 
 
-def DegreeInformed_extract():
-    filename = "DegreeInformed.txt"
-    # wait for file
-    filepath = wait_for_qoi(filename)
-    # replace it with file extraction
-    df_r = get_DegreeInformed_Dataframe(filepath)
-    df_r.to_csv(os.path.join(os.path.dirname(filepath), "DegreeInformed_extract.txt"), sep=" ")
+    def Time95Informed(self):
+        filename = "DegreeInformed.txt"
+        # wait for file
+        filepath = self.wait_for_qoi(filename)
+        # replace it with file extraction
+        df_r = self.get_DegreeInformed_Dataframe(filepath)
 
-    # plot
-    plt.plot(df_r.iloc[:, 0], df_r.iloc[:, 3])
-    plt.axhline(y=0.95, c="r")
-    plt.xlabel("Time [s] (Time = 0s : start of information dissemination)")
-    plt.ylabel("Percentage of pedestrians informed [%]")
-    plt.title("Information degree")
-    plt.savefig(os.path.join(os.path.dirname(filepath), "InformationDegree.png"))
+        dt = 0.4
+        time95 = 0.0
+        for perc in df_r.iloc[:, 3]:
+            if perc >= 0.95:
+                break
+            time95 += dt
 
-
-def Time95Informed():
-    filename = "DegreeInformed.txt"
-    # wait for file
-    filepath = wait_for_qoi(filename)
-    # replace it with file extraction
-    df_r = get_DegreeInformed_Dataframe(filepath)
-
-    dt = 0.4
-    time95 = 0.0
-    for perc in df_r.iloc[:, 3]:
-        if perc >= 0.95:
-            break
-        time95 += dt
-
-    f = open(os.path.join(os.path.dirname(filepath), "Time95Informed.txt"), "x")
-    f.write(f" timeToInform95PercentAgents\n0 {time95}")
-    f.close()
+        f = open(os.path.join(os.path.dirname(filepath), "Time95Informed.txt"), "x")
+        f.write(f" timeToInform95PercentAgents\n0 {time95}")
+        f.close()
 
 
-def post_processing():
-    DegreeInformed_extract()
-    Time95Informed()
+    def post_processing(self):
+        for q in self.qoi:
+           eval("self."+q+"()")
+
+        if self.remove is True:
+            self.clean_dir()
+
+    def pre_processing(self):
+        pass
+
+    def clean_dir(self):
+
+        shutil.rmtree("vadere")
+        shutil.rmtree("sumo")
+
+        fileList = []
+        for root, d_names, f_names in os.walk(os.getcwd()):
+            for f in f_names:
+                fileList.append(os.path.join(root, f))
+
+        for filePath in fileList:
+            fileName = os.path.basename(filePath)
+            if os.path.splitext(fileName)[0] not in self.qoi:
+                os.remove(filePath)
 
 
-def pre_processing():
-    shutil.rmtree(os.path.join(os.getcwd(), "results"), ignore_errors=True)
 
 
-def clean_dir():
-    patterns = [
-        "*.ini",
-        "*.scenario",
-        "*.sca",
-        "*.vci",
-        "*.vec",
-        "vadere",
-        "sumo",
-        "*.traj",
-        "*.csv",
-        "*.xml",
-        "*.sh",
-    ]
-    outputfolder_path = os.getcwd()
-    shutil.copytree(
-        outputfolder_path,
-        outputfolder_path + "temp",
-        ignore=shutil.ignore_patterns(*patterns),
-    )
-    shutil.rmtree(outputfolder_path)
-    os.rename(outputfolder_path + "temp", outputfolder_path)
 
 
 if __name__ == "__main__":
 
-    #print(sys.argv[0])  # prints python_script.py
-    #print(sys.argv[1])  # prints python_script.py
-    #print(sys.argv[2])  # prints python_script.py
+    sim_storage = "/home/christina/repos/rover-main/rover/simulations/simple_detoure_suqc"
 
+    # parse arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--qoi", action="append",nargs='+', help="specify qoi files")
+    parser.add_argument("--qoi", action="append",nargs='+', help="specify qoi files", type=str)
+    parser.add_argument('--remove', dest='keep', action='store_true')
+    parser.add_argument('--keep', dest='keep', action='store_false')
     args = parser.parse_args()
 
+    qoi = ["DegreeInformed_extract.txt","Time95Informed.txt"]
     if args.qoi:
-        for qoi in args.qoi:
-            print(qoi)
+        qoi = list(args.qoi[0])
+    qoi = [os.path.splitext(q)[0] for q in qoi]
 
+    remove = False
+    if args.keep:
+        remove = args.keep
+    if os.getcwd() == sim_storage:
+        print("Cannot delete original definition of simulation.")
+        remove = False
 
+    # start simulation
+    simulation_run = SimulationRun(qoi=qoi, remove = remove)
     os.environ["ROVER_MAIN"] = "/home/christina/repos/rover-main"
-    clean = False
+
     try:
-        pre_processing()
-        run_sim()
-        post_processing()
-        if clean is True:
-            clean_dir()
+        simulation_run.pre_processing()
+        simulation_run.run_sim()
+        simulation_run.post_processing()
     except:
         sys.exit(-1)
     sys.exit(0)  # ok
