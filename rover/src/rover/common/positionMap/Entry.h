21564803ab3453b3a6cbfae081b4ebcec58a4a8a
Stefan Schuhbaeck <stefan.schuhbaeck@hm.edu>
2020-09-16 16:16:02 +0200

    add test for IEntry
    Former-commit-id: 6ac65269f48e3300b3cec846c6d66558921af1f6

--- rover/src/rover/common/positionMap/Entry.h
+++ rover/src/rover/common/positionMap/Entry.h
@@ -42,6 +42,7 @@ class IEntry {
     count = 0;
     _valid = false;
   }
+  const bool empty() const;
   virtual const bool valid() const { return _valid; }
   virtual void incrementCount(const time_type& t);
 
@@ -65,16 +66,21 @@ class IEntry {
 /// implementation IEntry<K, T>
 
 template <typename K, typename T>
-IEntry<K, T>::IEntry()
+inline IEntry<K, T>::IEntry()
     : count(0), measurement_time(), received_time(), _valid(false) {}
 
 template <typename K, typename T>
-IEntry<K, T>::IEntry(const int count, const time_type& m_t,
-                     const time_type& r_t)
+inline IEntry<K, T>::IEntry(const int count, const time_type& m_t,
+                            const time_type& r_t)
     : count(count), measurement_time(m_t), received_time(r_t), _valid(true) {}
 
 template <typename K, typename T>
-void IEntry<K, T>::incrementCount(const time_type& t) {
+inline const bool IEntry<K, T>::empty() const {
+  return count < 0;
+}
+
+template <typename K, typename T>
+inline void IEntry<K, T>::incrementCount(const time_type& t) {
   count++;
   measurement_time = t;
   received_time = t;
@@ -82,22 +88,24 @@ void IEntry<K, T>::incrementCount(const time_type& t) {
 }
 
 template <typename K, typename T>
-const typename IEntry<K, T>::time_type& IEntry<K, T>::getMeasureTime() const {
+inline const typename IEntry<K, T>::time_type& IEntry<K, T>::getMeasureTime()
+    const {
   return measurement_time;
 }
 
 template <typename K, typename T>
-const typename IEntry<K, T>::time_type& IEntry<K, T>::getReceivedTime() const {
+inline const typename IEntry<K, T>::time_type& IEntry<K, T>::getReceivedTime()
+    const {
   return received_time;
 }
 
 template <typename K, typename T>
-const int IEntry<K, T>::getCount() const {
+inline const int IEntry<K, T>::getCount() const {
   return count;
 }
 
 template <typename K, typename T>
-int IEntry<K, T>::compareMeasureTime(const IEntry& other) const {
+inline int IEntry<K, T>::compareMeasureTime(const IEntry& other) const {
   if (measurement_time == other.measurement_time) return 0;
   if (measurement_time < other.measurement_time) {
     return -1;
@@ -107,7 +115,7 @@ int IEntry<K, T>::compareMeasureTime(const IEntry& other) const {
 }
 
 template <typename K, typename T>
-int IEntry<K, T>::compareReceivedTime(const IEntry& other) const {
+inline int IEntry<K, T>::compareReceivedTime(const IEntry& other) const {
   if (received_time == other.received_time) return 0;
   if (received_time < other.received_time) {
     return -1;
--- rover/tests/gtest/src/rover/common/positionMap/DensityMeasure_test.cc
+++ rover/tests/gtest/src/rover/common/positionMap/DensityMeasure_test.cc
@@ -12,11 +12,28 @@
 // rest must also rest nodeIds set
 TEST(LocalDensityMeasure, rest) {
   LocalDensityMeasure<int> m{};
-  ASSERT_FALSE(m.valid());
+  EXPECT_FALSE(m.valid());
   m.incrementCount(4.9);
   m.nodeIds.insert(32);
-  ASSERT_TRUE(m.valid());
+  EXPECT_TRUE(m.valid());
   m.reset();
-  ASSERT_EQ(0, m.nodeIds.size());
-  ASSERT_FALSE(m.valid());
+  EXPECT_EQ(0, m.nodeIds.size());
+  EXPECT_FALSE(m.valid());
+}
+
+TEST(LocalDensityMeasure, csv) {
+  LocalDensityMeasure<int> m{1, 3.5, 3.7};
+  std::string csv = "1,3.5,3.7";
+  EXPECT_STREQ(csv.c_str(), m.csv(",").c_str());
+  csv = "1;3.5;3.7";
+  EXPECT_STREQ(csv.c_str(), m.csv(";").c_str());
+}
+
+TEST(LocalDensityMeasure, empty) {
+  LocalDensityMeasure<int> m1{-1, 3.5, 3.7};
+  EXPECT_TRUE(m1.empty());
+  LocalDensityMeasure<int> m2{0, 3.5, 3.7};
+  EXPECT_FALSE(m2.empty());
+  LocalDensityMeasure<int> m3{1, 3.5, 3.7};
+  EXPECT_FALSE(m3.empty());
 }
