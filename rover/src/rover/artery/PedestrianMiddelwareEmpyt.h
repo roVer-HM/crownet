0c66d38562dfae25d5fc1f6e7a5ba405486d3309
Stefan Schuhbaeck <stefan.schuhbaeck@hm.edu>
2020-12-10 16:15:29 +0100

    add PedestrianMiddelwareEmpty
    Empty middelware without any additional logic. It only provides
    access to Idenity and Faciclity objects. Module does not
    manges self messages.
    
    
    Former-commit-id: 23d0b13e738e9962ead4790da8717051c03bc100

---
+++ rover/src/rover/artery/PedestrianMiddelwareEmpyt.h
@@ -0,0 +1,37 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+//
+
+#pragma once
+
+#include <artery/application/Middleware.h>
+#include <artery/application/MovingNodeDataProvider.h>
+#include "rover/artery/traci/VaderePersonController.h"
+
+namespace rover {
+class PedestrianMiddelwareEmpyt : public artery::MiddlewareBase {
+  virtual ~PedestrianMiddelwareEmpyt() = default;
+  void initialize(int stage) override;
+
+ protected:
+  void finish() override;
+  void initializeController(omnetpp::cPar&);
+  void receiveSignal(omnetpp::cComponent*, omnetpp::simsignal_t,
+                     omnetpp::cObject*, omnetpp::cObject*) override;
+
+ private:
+  VaderePersonController* mPersonController = nullptr;
+  artery::MovingNodeDataProvider mDataProvider;
+};
+}  // namespace rover
