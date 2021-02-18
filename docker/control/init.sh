266d5cd629687ead7fec6a9470c66a6d1ab54704
Christina <mayr9@hm.edu>
2021-02-18 03:17:07 +0100

    add third docker container setup

---
+++ docker/control/init.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#
+
+echo "Welcome to the roVer flowcontrol Docker Container."
+echo ""
+echo "Using TRACI_PORT='$TRACI_PORT' TRACI_DEBUG='$TRACI_DEBUG' CONTROL_LOG_LEVEL='$CONTROL_LOG_LEVEL'  CONTROL_LOG='$CONTROL_LOG'"
+echo ""
+
+
+
+# print control version
+crownetctl --version
+
+CMD_ARR=(crownetctl)
+CMD_ARR+=(--bind 0.0.0.0)
+# default to traci port 9997 to allow simultanous use of vadere and sumo and control
+if [[ -z $TRACI_PORT ]];then
+  CMD_ARR+=(--port 9997)
+else
+  CMD_ARR+=(--port $TRACI_PORT)
+fi
+
+if [[ ! -z $TRACI_DEBUG ]];then
+  CMD_ARR+=(--log "DEBUG")
+  CMD_ARR+=(--controller-log)
+fi
+
+
+
+if [[ ! -z $CONTROLLER_LOG_LEVEL ]];then
+    CMD_ARR+=(--controller-log-level $VADERE_LOG_LEVEL)
+fi
+
+if [[ ! -z $CONTROLLER_LOG ]];then
+    CMD_ARR+=(--controller-log $VADERE_LOG)
+fi
+
+
+# echo "${CMD_ARR[@]}"
+eval ${CMD_ARR[@]}
+
+echo "Container terminated."
---
+++ docker/control/update_container_with_branch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+CONTAINER='flowcontrol'
+VERSION_TAG='latest'
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -z "$1" ]
+then
+      echo "\$1 is empty - branch not specified."
+      exit -1
+else
+      $DIR/../../scripts/upd_container.sh $CONTAINER $VERSION_TAG -t "$IMAGE:branch_$1" --build-arg BRANCH=$1
+fi
+
