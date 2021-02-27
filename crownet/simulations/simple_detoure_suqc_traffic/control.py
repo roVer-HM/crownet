d620b78a2cdceaff370f3a227428b5bcb611e631
Christina <mayr9@hm.edu>
2021-02-27 11:04:48 +0100

    get mode automatically

--- crownet/simulations/simple_detoure_suqc_traffic/control.py
+++ crownet/simulations/simple_detoure_suqc_traffic/control.py
@@ -1,27 +1,37 @@
-import sys
+import sys, os
+
 from flowcontrol.crownetcontrol.controller.dummy_controller import Controller, TikTokController
 from flowcontrol.crownetcontrol.traci import constants_vadere as tc
-import logging
 
-from flowcontrol.crownetcontrol.traci.connection_manager import ClientModeConnection
+from flowcontrol.crownetcontrol.traci.connection_manager import ControlTraciWrapper
 from flowcontrol.crownetcontrol.traci.subsciption_listners import VaderePersonListener
 
 
-# TODO parse host, name, port
+if __name__ == "__main__":
 
-# class ScenarioControl(Controller):
-# TikTokController ->
-#	pass
-
-def server_test():
-	sub = VaderePersonListener.with_vars("persons", {"pos": tc.VAR_POSITION, "target_list": tc.VAR_TARGET_LIST})
+	sub = VaderePersonListener.with_vars(
+		"persons", {"pos": tc.VAR_POSITION, "target_list": tc.VAR_TARGET_LIST}
+	)
 	controller = TikTokController()
-	traci_manager = ClientModeConnection(control_handler=controller, port=9999)
+
+	if len(sys.argv) == 1:
+		settings = [
+			"--port",
+			"9999",
+			"--host-name",
+			"vadere",
+			"--client-mode"
+		]
+
+		traci_manager = ControlTraciWrapper.get_controller_from_args(
+			working_dir=os.getcwd(), args=settings, controller=controller)
+	else:
+		traci_manager = ControlTraciWrapper.get_controller_from_args(
+			working_dir=os.path.dirname(os.path.abspath(__file__)),
+			controller=controller)
+
 	controller.initialize_connection(traci_manager)
 	controller.start_controller()
 
 
-if __name__ == "__main__":
-	print(sys.argv)
-	server_test()
 	print()
