c5a7529effed248625af90edc8288171e59c08b2
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2021-03-22 17:04:42 +0100

    replace dircmp with md5sum digest comparison

--- crownet/simulations/guiding_crowds_test/test_run_simulation.py
+++ crownet/simulations/guiding_crowds_test/test_run_simulation.py
@@ -1,13 +1,12 @@
-import subprocess
 import os
-import filecmp
+import subprocess
+
+import roveranalyzer.utils.dirdiff as diff
 
 timeout_sec = 60
 
 
 def test_1():
-
-	# run vadere only
 
 	subprocess_cmd = ["python3", "run_script.py"]
 	settings = [
@@ -23,10 +22,17 @@ def test_1():
 		subprocess_cmd, timeout=timeout_sec, stderr=subprocess.PIPE
 	)
 
-	output_dir = os.path.abspath("results/vadere_only_test_1/vadere.d")
-	test_dir = os.path.abspath("tests/vadere_only_test_1/vadere.d")
+	output_dir = os.path.abspath("results/vadere_only_test_1/vadere.d/**")
+	right = diff.create_dir_diff(output_dir, os.path.abspath("results"))
 
-	assert len(filecmp.dircmp(output_dir, test_dir).diff_files) == 0
+	test_dir = os.path.abspath("tests/vadere_only_test_1/digest.csv")
+	left = diff.csv_to_diffdict(test_dir)
+
+	if not diff.compare(left, right):
+		# write new digest to swap if correct
+		diff.diffdict_to_csv(test_dir + ".UPDATE", right)
+		assert False
+
 
 def test_2():
 
@@ -47,10 +53,15 @@ def test_2():
 		subprocess_cmd, timeout=timeout_sec, stderr=subprocess.PIPE
 	)
 
-	output_dir = os.path.abspath("results/final_test_2/vadere.d")
-	test_dir = os.path.abspath("tests/final_test_2/vadere.d")
+	output_dir = os.path.abspath("results/final_test_2/vadere.d/**")
+	right = diff.create_dir_diff(output_dir, os.path.abspath("results"))
+	test_dir = os.path.abspath("tests/final_test_2/digest.csv")
+	left = diff.csv_to_diffdict(test_dir)
 
-	assert len(filecmp.dircmp(output_dir, test_dir).diff_files) == 0
+	if not diff.compare(left, right):
+		# write new digest to swap if correct
+		diff.diffdict_to_csv(os.path.join(test_dir, "digest.csv.UPDATE"), right)
+		assert False
 
 def test_3():
 
@@ -73,10 +84,15 @@ def test_3():
 		subprocess_cmd, timeout=timeout_sec, stderr=subprocess.PIPE
 	)
 
-	output_dir = os.path.abspath("results/final_control_test_3/vadere.d")
-	test_dir = os.path.abspath("tests/final_control_test_3/vadere.d")
+	output_dir = os.path.abspath("results/final_control_test_3/vadere.d/**")
+	right = diff.create_dir_diff(output_dir, os.path.abspath("results"))
+	test_dir = os.path.abspath("tests/final_control_test_3/digest.csv")
+	left = diff.csv_to_diffdict(test_dir)
 
-	assert len(filecmp.dircmp(output_dir, test_dir).diff_files) == 0
+	if not diff.compare(left, right):
+		# write new digest to swap if correct
+		diff.diffdict_to_csv(os.path.join(test_dir, "digest.csv.UPDATE"), right)
+		assert False
 
 
 def test_4():
@@ -99,10 +115,15 @@ def test_4():
 		subprocess_cmd, timeout=timeout_sec, stderr=subprocess.PIPE
 	)
 
-	output_dir = os.path.abspath("results/vadere_controlled_test_4/vadere.d")
-	test_dir = os.path.abspath("tests/vadere_controlled_test_4/vadere.d")
+	output_dir = os.path.abspath("results/vadere_controlled_test_4/vadere.d/**")
+	right = diff.create_dir_diff(output_dir, os.path.abspath("results"))
+	test_dir = os.path.abspath("tests/vadere_controlled_test_4/digest.csv")
+	left = diff.csv_to_diffdict(test_dir)
 
-	assert len(filecmp.dircmp(output_dir, test_dir).diff_files) == 0
+	if not diff.compare(left, right):
+		# write new digest to swap if correct
+		diff.diffdict_to_csv(os.path.join(test_dir, "digest.csv.UPDATE"), right)
+		assert False
 
 
 def test_5():
@@ -124,14 +145,22 @@ def test_5():
 		subprocess_cmd, timeout=timeout_sec, stderr=subprocess.PIPE
 	)
 
-	output_dir = os.path.abspath("results/vadere_only_signs_test_5/vadere.d")
-	test_dir = os.path.abspath("tests/vadere_only_signs_test_5/vadere.d")
+	output_dir = os.path.abspath("results/vadere_only_signs_test_5/vadere.d/**")
+	right = diff.create_dir_diff(output_dir, os.path.abspath("results"))
+	test_dir = os.path.abspath("tests/vadere_only_signs_test_5/digest.csv")
+	left = diff.csv_to_diffdict(test_dir)
 
-	assert len(filecmp.dircmp(output_dir, test_dir).diff_files) == 0
+	if not diff.compare(left, right):
+		# write new digest to swap if correct
+		diff.diffdict_to_csv(os.path.join(test_dir, "digest.csv.UPDATE"), right)
+		assert False
 
 if __name__=="__main__":
-	test_1()
-	test_2()
-	test_3()
-	test_4()
-	test_5()
+	# test_1()
+	# test_2()
+	# test_3()
+	# test_4()
+	# test_5()
+	output_dir = os.path.abspath("tests/final_control_test_3/**")
+	r = diff.create_dir_diff(output_dir, os.path.abspath("tests"))
+	diff.diffdict_to_csv("tests/final_control_test_3/digest_all.csv", r)
