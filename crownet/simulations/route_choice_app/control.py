af4f6aa22ab77dd80c9cfb3013a92a7d3f4cb09b
Christina <mayr9@hm.edu>
2021-06-09 16:27:15 +0200

    update scenario

--- crownet/simulations/route_choice_app/control.py
+++ crownet/simulations/route_choice_app/control.py
@@ -1,4 +1,5 @@
 import sys, os
+import json
 
 from flowcontrol.crownetcontrol.setup.entrypoints import get_controller_from_args
 from flowcontrol.crownetcontrol.setup.vadere import get_scenario_content
@@ -10,7 +11,6 @@ from flowcontrol.strategy.controller.dummy_controller import Controller
 from flowcontrol.crownetcontrol.traci import constants_vadere as tc
 from flowcontrol.utils.misc import get_scenario_file
 
-from shapely import geometry
 
 
 class DistributeAgentsOverCorridors(Controller):
@@ -22,7 +22,7 @@ class DistributeAgentsOverCorridors(Controller):
         self.radius = 7.5
         self.next_call = 0
         self.counter = 0
-        self.target_ids = ["11", "21", "31", "41", "51"]
+        self.target_ids = [11, 21, 31, 41, 51]
 
     def in_circle(self, x, y):
         square_dist = (self.x_center - x) ** 2 + (self.y_center - y) ** 2
@@ -36,11 +36,11 @@ class DistributeAgentsOverCorridors(Controller):
 
         if self.counter >=5:
             self.counter = 0
-        target_id = self.target_ids[self.counter]
+        target_id = str(self.target_ids[self.counter])
 
         peds1 = self.con_manager.sub_listener["default"].pedestrians
         ids = [p["id"] for p in peds1 if self.in_circle(p["pos"][0],p["pos"][1] )]
-        print(f"Set target {target_id}, Ids: {ids}")
+        print(f"Simulation time: {self.next_call} \t Set target {target_id}, Ids: {ids}")
         for ped_id in ids:
             self.con_manager.domains.v_person.set_target_list(
                 str(ped_id), [target_id]
@@ -56,17 +56,42 @@ class DistributeAgentsOverCorridors(Controller):
         print(sim_state)
 
 
+class DistributeAgentsOverCorridorsInform(DistributeAgentsOverCorridors, Controller):
+
+    def __init__(self):
+        super().__init__()
+
+    def handle_sim_step(self, sim_time, sim_state):
+
+        if self.counter >=5:
+            self.counter = 0
+        target_id = self.target_ids[self.counter]
+
+
+        command = {"targetIds" : [target_id] , "probability" : [1.0]}
+        action = {"time" : sim_time+0.4, "space" : {"x": self.x_center, "y" : self.y_center, "radius": self.radius}, "command" : command}
+        action = json.dumps(action)
+
+        print(f"Simulation time: {self.next_call} \t provide information")
+        self.con_manager.domains.v_sim.send_control(message=action, model="RouteChoice")
+
+        self.next_call += 30
+        self.con_manager.next_call_at(self.next_call)
+        self.counter += 1
+
+
 if __name__ == "__main__":
 
     if len(sys.argv) == 1:
-        settings = ["--port", "9999", "--host-name", "localhost", "--client-mode", "--start-server", "--controller-type", "PingPong", "--gui-mode"]
+        settings = ["--port", "9999", "--host-name", "localhost", "--client-mode", "--start-server", "--gui-mode"]
     else:
         settings = sys.argv[1:]
 
-
-    controller = DistributeAgentsOverCorridors()
-
     scenario_file = get_scenario_file("vadere/scenarios/simplified_default_sequential.scenario")
+    kwargs = {
+        "file_name": scenario_file,
+        "file_content": get_scenario_content(scenario_file),
+    }
 
     sub = VadereDefaultStateListener.with_vars(
         "persons",
@@ -74,14 +99,16 @@ if __name__ == "__main__":
         init_sub=True,
     )
 
-    controller = get_controller_from_args(
-        working_dir=os.getcwd(), args=settings, controller= controller
-    )
+    for control in ['DistributeAgentsOverCorridors','DistributeAgentsOverCorridorsInform']:
 
-    kwargs = {
-        "file_name": scenario_file,
-        "file_content": get_scenario_content(scenario_file),
-    }
-    controller.register_state_listener("default", sub, set_default=True)
-    controller.start_controller(**kwargs)
+        settings_ = settings
+        settings_.extend(["--output-dir", os.path.join(os.getcwd(), control)])
+        settings_.extend(["--controller-type", control])
+
+        controller = get_controller_from_args(
+            working_dir=os.getcwd(), args=settings
+        )
+
+        controller.register_state_listener("default", sub, set_default=True)
+        controller.start_controller(**kwargs)
 
