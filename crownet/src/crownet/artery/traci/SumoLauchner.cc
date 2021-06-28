e0ed6f309eb202c91604be1f6471e644bf958df0
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2021-06-28 10:48:22 +0200

    create launch.xml from sumo.cfg.xml

--- crownet/src/crownet/artery/traci/SumoLauchner.cc
+++ crownet/src/crownet/artery/traci/SumoLauchner.cc
@@ -8,7 +8,6 @@
 #include "crownet/artery/traci/SumoLauchner.h"
 #include "crownet/crownet.h"
 
-#include <boost/filesystem.hpp>
 namespace fs = boost::filesystem;
 
 using namespace traci;
@@ -38,7 +37,6 @@ void SumoLauchner::initialize()
 }
 
 void SumoLauchner::initializeServer(std::shared_ptr<API> api){
-
     int seed = par("seed").intValue();
     if (seed == -1) {
         seed = uniform(0, std::numeric_limits<int>::max()); //
@@ -50,23 +48,48 @@ void SumoLauchner::initializeServer(std::shared_ptr<API> api){
     fs::path iniBaseDir = fs::current_path();
     fs::path cfgFilePath (par("sumoCfgBase").stdstringValue());
     cfgFilePath = cfgFilePath.is_relative() ? fs::absolute(cfgFilePath, iniBaseDir) : cfgFilePath;
+    bool deleteXML = false;
 
-    // add seed child
     cXMLElement* cfgFile = getEnvir()->getXMLDocument(cfgFilePath.c_str());
+    if (std::strcmp("launch", cfgFile->getName()) == 0){
+        // launch.xml provided. Just use it.
+    } else if (std::strcmp("configuration", cfgFile->getName()) == 0){
+        // configuration file provided. Build launch xml
+        cXMLElement* lauchnXml = new cXMLElement("launch", nullptr);
+
+        auto inputs = cfgFile->getChildrenByTagName("input")[0];
+        for(cXMLElement* input : inputs->getChildren()){
+            cXMLElement* file = new cXMLElement("copy", lauchnXml);
+            file->setAttribute("file", input->getAttribute("value"));
+        }
+
+        cXMLElement* file = new cXMLElement("copy", lauchnXml);
+        file->setAttribute("file", cfgFilePath.filename().c_str());
+        file->setAttribute("type", "config");
+        cfgFile = lauchnXml;
+        deleteXML = true;
+    } else {
+        throw cRuntimeError("expected launch or configuration root element got %s", cfgFile->getName());
+    }
+
     cXMLElement* seed_node = new cXMLElement("seed", cfgFile);
     seed_node->setAttribute("value", ss.str().c_str());
     cfgFile->appendChild(seed_node);
 
-    // add basedir child
+
     cXMLElement* basedir_node = new cXMLElement("basedir", cfgFile);
     basedir_node->setAttribute("path", cfgFilePath.parent_path().c_str());
     cfgFile->appendChild(basedir_node);
+
 
     std::string xmlString = cfgFile->getXML();
 
     // cfgFilePath.filename().string()
     // fixme: veins dispatcher script expects this name.
     api->sendFile("sumo-launchd.launch.xml", xmlString);
+    if (deleteXML)
+        delete cfgFile; // in case we created a new xml file manually delete it here
 }
+
 
 } /* namespace crownet */
