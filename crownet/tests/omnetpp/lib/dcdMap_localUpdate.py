5b754840138200b68c23f4a280a94292bd4c8942
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2021-12-10 11:30:21 +0100

    fix opp-tests

--- crownet/tests/omnetpp/lib/dcdMap_localUpdate.py
+++ crownet/tests/omnetpp/lib/dcdMap_localUpdate.py
@@ -5,17 +5,15 @@ import sys
 
 # from roveranalyzer.simulators.crownet.dcd.dcd_builder import DcdBuilder
 
-import roveranalyzer.simulators.opp as opp
-import roveranalyzer.simulators.crownet.dcd as Dmap
-from roveranalyzer.simulators.crownet.dcd.util import remove_not_selected_cells
+import roveranalyzer.simulators.crownet.dcd.dcd_builder as Builders
 from roveranalyzer.simulators.opp.provider.hdf.DcdMapProvider import DcdMapProvider
 
 def dcd_map_localUpdate(hostId, data_root):
     print(data_root)
     data_root = os.path.abspath(data_root)
     print(data_root)
-    builder = Dmap.DcdHdfBuilder.get("data.h5", data_root)
-    map: DcdMapProvider = builder.build()["map_p"]
+    builder = Builders.DcdHdfBuilder.get("data.h5", data_root)
+    map: DcdMapProvider = builder.build().map_p
     
     host_df = map[I[:, :, :, :, hostId], ["count", "selection"]]
     host_df = host_df[host_df["selection"] != 0].copy(deep=True)
