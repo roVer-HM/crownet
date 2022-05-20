f06572192a72281d763f9b0277b6a1302a957391
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2022-05-20 09:31:00 +0200

    add scripts

--- crownet/simulations/densityMap/analysis/a_mf_stationary_m.py
+++ crownet/simulations/densityMap/analysis/a_mf_stationary_m.py
@@ -4,21 +4,18 @@
 
 
 
-from roveranalyzer.analysis.common import RunContext, Simulation
+from roveranalyzer.analysis.common import RunContext, Simulation, SuqcRun
 from roveranalyzer.simulators.opp.scave import CrownetSql
 
 
-def main(ctx: RunContext):
-    sim: Simulation  = Simulation.from_context(ctx)
-    sql: CrownetSql =  sim.sql
-    enbs = sql.vector_ids_to_host(
-        sql.m_phy(), "servingCell:vector", name_columns=["hostId"], pull_data=True
-    )
-    enbs = enbs.drop(columns=["vectorId"]).rename(columns={"value": "eNB"})
-    enbs = enbs.set_index(["hostId", "time"]).sort_index()
-    print("done")
+def main(run: SuqcRun, idx=0):
+    sim: Simulation = run.get_sim(idx)
+    dmap = sim.get_dcdMap()
+    fig, ax  = dmap.plot_count_diff()
+    print("foo")
     
 
 
 if __name__ == "__main__":
-    main(ctx=RunContext.from_path("/mnt/data1tb/results/mf_stationary_m/simulation_runs/Sample__5_0/runContext.json"))
+    run = SuqcRun("/mnt/data1tb/results/mf_stationary_m_single_cell_3/")
+    main(run=run , idx=0)
