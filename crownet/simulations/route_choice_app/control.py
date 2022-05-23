6a6dffda8e78a368b0b4c7aec01e4c378ce73b64
Christina <mayr9@hm.edu>
2022-05-23 14:01:49 +0200

    update scenario + control

--- crownet/simulations/route_choice_app/control.py
+++ crownet/simulations/route_choice_app/control.py
@@ -2,6 +2,8 @@ import os
 import sys
 import json
 
+from shapely.geometry import Polygon
+
 from flowcontrol.crownetcontrol.setup.entrypoints import get_controller_from_args
 from flowcontrol.crownetcontrol.state.state_listener import VadereDefaultStateListener
 from flowcontrol.crownetcontrol.traci.connection_manager import ClientModeConnection, ServerModeConnection
@@ -14,19 +16,15 @@ sys.path.append(os.path.abspath(".."))
 from flowcontrol.crownetcontrol.controller import Controller
 from flowcontrol.crownetcontrol.traci import constants_vadere as tc
 
-from shapely.geometry import Polygon
-
-PRECISION = 8
-
-
 from flowcontrol.strategy.timestepping.timestepping import FixedTimeStepper
+from flowcontrol.strategy.controller.control_algorithm import AlternateTargetAlgorithm, MinimalDensityAlgorithm
 
 class NoController(Controller):
 
-    def __init__(self):
+    def __init__(self, control_alg=None):
         super().__init__(
-            control_algorithm=None,
-            time_stepper=FixedTimeStepper(time_step_size=10.0, start_time=4.0),
+            control_algorithm=control_alg,
+            time_stepper=FixedTimeStepper(time_step_size=10.0, start_time=10.0),
             processor_manager=Manager()
         )
         self.density_over_time = list()
@@ -34,17 +32,13 @@ class NoController(Controller):
     def handle_sim_step(self, sim_time, sim_state):
         self.processor_manager.update_sim_time(sim_time)
         self.measure_state(sim_time)
-
-        self.compute_next_corridor_choice(sim_time)
-
-
+        self.compute_next_corridor_choice()
         self.apply_redirection_measure()
         self.commandID += 1
-
         self.time_stepper.forward_time()
 
-        print("")
-
+    def compute_next_corridor_choice(self):
+        pass
 
     def measure_state(self, sim_time):
         if isinstance(self.con_manager, ServerModeConnection):
@@ -90,27 +84,32 @@ class NoController(Controller):
         pass
 
     def handle_init(self, sim_time, sim_state):
-        self.counter = -1
         self.processor_manager.registerProcessor("commandId", ControlActionCmdId(
             writer=Writer(os.path.join(self.output_dir, "commandIds.txt"))))
-
-    def compute_next_corridor_choice(self, sim_time):
-        pass
-
-    def set_reaction_model_parameters(self, reaction_probability):
-        pass
 
     def write_data(self):
         self.processor_manager.finish()
 
 
-class OpenLoop(Controller):
+class OpenLoop(NoController, Controller):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, control_alg = None):
         self.target_ids = [31, 21, 11]
         self.redirected_agents = list()
         self.commandID = 111
+        self.redirection_area = Rectangle(x=180., y=190., width=20., height=15.)
+
+        if control_alg == None:
+            control_alg= AlternateTargetAlgorithm(alternate_targets=self.target_ids)
+        super().__init__(control_alg=control_alg)
+
+
+    def get_stimulus_info(self):
+
+        location = Location(areas=self.redirection_area)
+        recommendation = InformationStimulus(f"use target [{self.current_target}]")
+        s = StimulusInfo(location=location, stimuli=recommendation)
+        return s
 
 
     def measure_state(self, sim_time):
@@ -123,44 +122,25 @@ class OpenLoop(Controller):
         self.processor_manager.post_loop("sending_times", commandIdsSent)
         super().write_data()
 
+
     def apply_redirection_measure(self):
-
-        rectangle = Rectangle(x=180., y=190., width=20., height=15.)
-        location = Location(areas=rectangle)
-        recommendation = InformationStimulus(f"use target [{self.target_ids[self.counter]}]")
-        s = StimulusInfo(location=location, stimuli=recommendation)
-        action0 = s.toJSON()
-
+        self.compute_next_corridor_choice()
+        recommendation = self.get_stimulus_info()
         self.processor_manager.write("commandId", self.commandID)
+
         if isinstance(self.con_manager, ServerModeConnection):
-            self.con_manager.domains.v_sim.send_control(message=action0, model=self.controlModelName,
-                                                        sending_node_id="misc[0].app[0]")
+            self.con_manager.domains.v_sim.send_control(message=recommendation.toJSON(), sending_node_id="misc[0].app[0]")
         else:
-            self.con_manager.domains.v_sim.send_control(message=action0, model=self.controlModelName)
+            self.con_manager.domains.v_sim.send_control(message=recommendation.toJSON())
 
-    def _increase_counter(self):
-        self.counter += 1
-        self._reset_counter()
-
-    def _reset_counter(self):
-        if self.counter >= len(self.target_ids):
-            self.counter = 0
-
-    def compute_next_corridor_choice(self, sim_time):
-        self.choose_corridor()
-        print(f"Use corridor {self.target_ids[self.counter]}.")
-        self.processor_manager.write("path_choice", self.target_ids[self.counter])
-
-    def choose_corridor(self):
-        self._increase_counter()
-
+    def compute_next_corridor_choice(self):
+        self.current_target = self.control_algorithm.get_next_target()
 
 
     def handle_init(self, sim_time, sim_state):
         super().handle_init(sim_time, sim_state)
-        self.con_manager.domains.v_sim.init_control(
-            self.controlModelName, self.controlModelType, self.get_reaction_model_parameters()
-        )
+        self.con_manager.domains.v_sim.init_control()
+
         self.processor_manager.registerProcessor("sending_times", SendingTimes(
             Writer(os.path.join(self.output_dir, "sending_times.txt"))))
         self.processor_manager.registerProcessor("path_choice", CorridorRecommendation(
@@ -169,14 +149,15 @@ class OpenLoop(Controller):
 
 class ClosedLoop(OpenLoop, Controller):
     def __init__(self):
-        super().__init__()
+        self.target_ids = [31, 21, 11]
+        control_alg = MinimalDensityAlgorithm(alternate_targets=self.target_ids)
+        super().__init__(control_alg=control_alg)
 
-    def choose_corridor(self):
-        densities = np.array(self.density_over_time[-1:]).mean(axis=0)
-        self.counter = np.argwhere(densities == densities.min()).ravel()[0]  # recommend shortest corridor
-        print(
-            f"The densities are: {densities.round(3)}. Minimum: index = {self.counter}."
-        )
+
+    def compute_next_corridor_choice(self):
+        self.current_target = self.control_algorithm.get_next_target(self.density_over_time[-1])
+        self.processor_manager.write("path_choice", self.current_target)
+
 
 
 if __name__ == "__main__":
@@ -184,7 +165,7 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         # default settings for control-vadere (no Omnetpp!)
         settings = ["--controller-type",
-                    "NoController",  #
+                    "OpenLoop",  #
                     "--scenario-file",
                     "route_choice_real_world.scenario",
                     "--port",
