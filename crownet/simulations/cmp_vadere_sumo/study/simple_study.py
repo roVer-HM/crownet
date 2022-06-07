b871e04b860ea879ad78e3ac0da70cc6721475ef
Lars Wischhof <wischhof@ieee.org>
2022-06-07 17:51:57 +0200

    estimate maximum number of parallel running sims

--- crownet/simulations/cmp_vadere_sumo/study/simple_study.py
+++ crownet/simulations/cmp_vadere_sumo/study/simple_study.py
@@ -2,7 +2,7 @@
 #
 # Launch this script via python3 or within your Python IDE to perform a simulation study.
 
-import os, shutil
+import os, shutil, psutil
 import string
 from typing import List, Dict, Any
 from enum import Enum
@@ -21,7 +21,12 @@ import pprint
 import random
 
 # number of repetitions to be performed for each parameter set
-REPS = 40
+REPS = 4
+
+# estimated amount of memory per simulation
+MEM_PER_SIM_VADERE_GB = 11
+MEM_PER_SIM_SUMO_GB = 1
+MEM_PER_SIM_OMNET_GB = 1
 
 
 class MobilitySimulatorType(Enum):
@@ -30,18 +35,19 @@ class MobilitySimulatorType(Enum):
     SUMO = 3
 
 
+def max_parallel_sims(mobility_sim: MobilitySimulatorType) -> int:
+    estimated_mem_per_sim = MEM_PER_SIM_OMNET_GB
+    if mobility_sim == MobilitySimulatorType.SUMO:
+        estimated_mem_per_sim = estimated_mem_per_sim + MEM_PER_SIM_SUMO_GB
+    elif mobility_sim == MobilitySimulatorType.VADERE:
+        estimated_mem_per_sim = estimated_mem_per_sim + MEM_PER_SIM_VADERE_GB
+
+    max_sims = int(((psutil.virtual_memory()[4]) / (1024 * 1024 * 1024)) / estimated_mem_per_sim)
+    return max_sims
+
+
 def main(base_path):
     time = UnitValue.s(600.0)
-
-    # only2: VADERE
-    par_var = [
-        {
-            "omnet": {
-                "sim-time-limit": time,
-                },
-        },
-    ]
-    run_parameter_study(base_path, MobilitySimulatorType.VADERE, "vadereOnly2", par_var)
 
     # only2: SUMO
     par_var = [
@@ -52,6 +58,18 @@ def main(base_path):
         },
     ]
     run_parameter_study(base_path, MobilitySimulatorType.SUMO, "sumoOnly2", par_var)
+
+    # only2: VADERE
+    par_var = [
+        {
+            "omnet": {
+                "sim-time-limit": time,
+            },
+        },
+    ]
+    run_parameter_study(base_path, MobilitySimulatorType.VADERE, "vadereOnly2", par_var)
+
+
 
 def run_parameter_study(base_path: str, mobility_sim: MobilitySimulatorType, config: str,
                         par_var: List[Dict[str, Any]]):
@@ -65,7 +83,7 @@ def run_parameter_study(base_path: str, mobility_sim: MobilitySimulatorType, con
     # 
     ini_file = os.path.abspath("../omnetpp.ini")
     study_name = __file__.replace(".py", "")
-    base_dir = os.path.abspath("../results/"+study_name+"/")
+    base_dir = os.path.abspath("../results/" + study_name + "/")
     os.makedirs(base_dir, exist_ok=True)
 
     if mobility_sim == MobilitySimulatorType.SUMO:
@@ -110,12 +128,12 @@ def run_parameter_study(base_path: str, mobility_sim: MobilitySimulatorType, con
         model=model,
         creator=opp_creator,
         rnd_hostname_suffix=f"_{_rnd}",
-        runscript_out=config+"_runscript.out"
+        runscript_out=config + "_runscript.out"
     )
     print("setup done")
-    # par_var, data = setup.run(len(par_var))
-    par_var, data = setup.run(20)
-
+    nr_parallel = max_parallel_sims (mobility_sim);
+    print(f'estimated maximum number of parallel simulations: {nr_parallel}')
+    par_var, data = setup.run(nr_parallel)
 
 if __name__ == "__main__":
     main("./")
