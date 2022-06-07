cae81570d0cde69625e23d625ad1e5ada363872e
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2022-06-07 15:40:33 +0200

    update postion script to create density based positions

---
+++ crownet/simulations/densityMap/pos_provider.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+function checkOK() {
+    if [ "$?" -ne 0 ]; then
+        echo "error in command"
+        exit -1
+    fi
+}
+
+NUM=5
+SCENARIO="vadere/scenarios/mf_m_dyn_const_4e20s_15x8_120.scenario"
+OUT="misc_pos2.ini"
+
+
+num=(10 26 50 76 100 126 150 176 200)
+
+# for n in ${num[@]}; do
+#     echo "Create with number of runs $n"
+#     rnd_stationary_positions \
+#         --vadere $SCENARIO \
+#         --number-of-runs $NUM \
+#         --number-of-positions ${n} \
+#         --seed-type order \
+        # --append \
+#         --output $OUT
+#       checkOK
+# done
+
+density=(0.000611583389395144 0.00076447923674393 0.0009173750840927161 \
+         0.001070270931441502 0.001223166778790288)
+
+run=0
+
+for n in ${density[@]}; do
+    echo "Create with density ${n} on full area $n"
+    rnd_stationary_positions \
+        --vadere $SCENARIO \
+        --number-of-runs $NUM \
+        --const-density ${n} \
+        --seed-type order \
+        --config-name "full_${run}" \
+        --append \
+        --output $OUT
+    checkOK
+
+    echo "Create with density ${n} on half area $n"
+    rnd_stationary_positions \
+        --vadere $SCENARIO \
+        --number-of-runs $NUM \
+        --const-density ${n} \
+        --seed-type order \
+        --offset 0.25 0.25 0.5 0.5 \
+        --config-name "quater_${run}" \
+        --append \
+        --output $OUT
+    checkOK
+    run=$(($run + 1))
+done
