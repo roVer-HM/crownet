a5af59fe24bdd46c82cd5267c0ef0599e8b52ee4
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2022-06-21 14:09:15 +0200

    format files

--- crownet/simulations/densityMap/analysis/a_mf_stationary_m_single_cell.py
+++ crownet/simulations/densityMap/analysis/a_mf_stationary_m_single_cell.py
@@ -29,50 +29,52 @@ def min_max_norm(data, min=0, max=None):
 
 
 def process_relative_err(df: pd.DataFrame):
-    if any (df.loc[pd.IndexSlice[:, :, "map_glb_count"], "std"] != 0):
+    if any(df.loc[pd.IndexSlice[:, :, "map_glb_count"], "std"] != 0):
         raise ValueError("WARNING: global number of nodes differs between merged runs")
-    
+
     df = df.rename(columns={"p_50": "median"})
-    df = df.unstack('data').swaplevel(-2, -1, axis=1)
+    df = df.unstack("data").swaplevel(-2, -1, axis=1)
     df = df.sort_index(axis=1)
-    df = df.drop(columns=[('map_glb_count', 'std'), ('map_glb_count', 'median')])
-    df = df.rename(columns={'map_mean_count':'map_count'})
+    df = df.drop(columns=[("map_glb_count", "std"), ("map_glb_count", "median")])
+    df = df.rename(columns={"map_mean_count": "map_count"})
     df.columns = [f"{l1}_{l2}" for l1, l2 in df.columns]
-    df = df.rename(columns={'map_glb_count_mean':'map_glb_count'})
-    
-    g = 'map_glb_count'
-    m = 'map_count_mean'
-    gn = 'map_glb_count_n'
-    mn = 'map_count_mean_n'
-    mm = 'map_count_median'
-    mmn = 'map_count_median_n'
+    df = df.rename(columns={"map_glb_count_mean": "map_glb_count"})
+
+    g = "map_glb_count"
+    m = "map_count_mean"
+    gn = "map_glb_count_n"
+    mn = "map_count_mean_n"
+    mm = "map_count_median"
+    mmn = "map_count_median_n"
     df[gn] = min_max_norm(df[g])
     df[mn] = min_max_norm(df[m], max=df[g].max())
     df[mmn] = min_max_norm(df[mm], max=df[g].max())
-    
-    
+
     df.columns.name = "data"
     df = df.stack().sort_index()
     return df
 
+
 class Rep:
     def __init__(self):
-       self.num = 0
-    
-    def __call__(self, count=3 ) -> List[int]:
-        ret = range(self.num, self.num+count)
+        self.num = 0
+
+    def __call__(self, count=3) -> List[int]:
+        ret = range(self.num, self.num + count)
         self.num += count
         return list(ret)
+
 
 def kwargs_map(fn, kwargs):
     try:
         print(f"Execute {kwargs['scenario_lbl']}")
-        ret = fn (**kwargs)
+        ret = fn(**kwargs)
         print(f"Execute {kwargs['scenario_lbl']} done.")
         return (True, ret)
     except Exception as e:
         print(f"Error in {kwargs['scenario_lbl']} message: {e}")
         return (False, f"Error in {kwargs['scenario_lbl']} message: {e}")
+
 
 def get_run_map(study: SuqcRun, rep_count=10):
     r = Rep()
@@ -82,17 +84,28 @@ def get_run_map(study: SuqcRun, rep_count=10):
         (0.00076447923674393, "7.6e-4", 128),
         (0.0009173750840927161, "9.2e-4", 152),
         (0.001070270931441502, "1.1e-3", 176),
-        (0.001223166778790288, "1.2e-3", 200)
+        (0.001223166778790288, "1.2e-3", 200),
     ]
-    
+
     run_map.update(
-        { f"ped_{run}_0": dict(rep=r(rep_count), lbl=f"ped_{run}_0") for run in [10, 26, 50, 76, 100, 126, 150, 176, 200] }
+        {
+            f"ped_{run}_0": dict(rep=r(rep_count), lbl=f"ped_{run}_0")
+            for run in [10, 26, 50, 76, 100, 126, 150, 176, 200]
+        }
     )
     run_map.update(
-        { f"415x394_{c}": dict(rep=r(rep_count), lbl=f"415x394_{c}", density=d) for d, _, c in densities }
+        {
+            f"415x394_{c}": dict(rep=r(rep_count), lbl=f"415x394_{c}", density=d)
+            for d, _, c in densities
+        }
     )
     run_map.update(
-        { f"207x196_{int(c/4)}": dict(rep=r(rep_count), lbl=f"207x196_{int(c/4)}", density=d) for d, _, c in densities }
+        {
+            f"207x196_{int(c/4)}": dict(
+                rep=r(rep_count), lbl=f"207x196_{int(c/4)}", density=d
+            )
+            for d, _, c in densities
+        }
     )
 
     return run_map
@@ -104,58 +117,84 @@ def read_data(study: SuqcRun):
 
     merged_norm_path = os.path.join(study.base_path, "merged_normalized_map_measure.h5")
     if os.path.exists(merged_norm_path):
-        map = pd.DataFrame(pd.HDFStore(merged_norm_path, mode="r").get("map_measure_norm_static"))
+        map = pd.DataFrame(
+            pd.HDFStore(merged_norm_path, mode="r").get("map_measure_norm_static")
+        )
     else:
 
         _runs = run_map.items()
-        data=["map_glb_count", "map_mean_count"]
+        data = ["map_glb_count", "map_mean_count"]
         kwargs_iter = [
-            {'study': s, 'scenario_lbl':run[0], 'rep_ids': run[1]["rep"], 'data':d, 'frame_consumer':c}
-            for s, run, d, c in 
-            zip(repeat(study), _runs, repeat(data), repeat(process_relative_err) )
+            {
+                "study": s,
+                "scenario_lbl": run[0],
+                "rep_ids": run[1]["rep"],
+                "data": d,
+                "frame_consumer": c,
+            }
+            for s, run, d, c in zip(
+                repeat(study), _runs, repeat(data), repeat(process_relative_err)
+            )
         ]
         with get_context("spawn").Pool(10) as pool:
-            maps = pool.starmap(kwargs_map, zip(repeat(OppAnalysis.merge_maps), kwargs_iter))
+            maps = pool.starmap(
+                kwargs_map, zip(repeat(OppAnalysis.merge_maps), kwargs_iter)
+            )
         # map = OppAnalysis.merge_maps(**kwargs_iter[0])
 
         for ret, map in maps:
             if not ret:
                 print(map)
-        
+
         maps = [map for ret, map in maps if ret]
         map: pd.DataFrame = pd.concat(maps, axis=0, verify_integrity=True)
         map.to_hdf(merged_norm_path, key="map_measure_norm_static", mode="a")
 
-    return map 
+    return map
 
 
+def get_convergence_time(map: pd.DataFrame, time_slice, err) -> dict:
 
-def get_convergence_time(map:pd.DataFrame, time_slice, err) -> dict:
-
-    df = map.loc[pd.IndexSlice[:, time_slice, ("map_count_mean_n", "map_glb_count_n")],].unstack("data").copy(deep=True)
+    df = (
+        map.loc[
+            pd.IndexSlice[:, time_slice, ("map_count_mean_n", "map_glb_count_n")],
+        ]
+        .unstack("data")
+        .copy(deep=True)
+    )
     if isinstance(df.columns, pd.MultiIndex):
         df = df.droplevel(0, axis=1)
-    mask = df["map_count_mean_n"] >= ( df["map_glb_count_n"] - err )
-    mask = mask &  (df["map_count_mean_n"] <= ( df["map_glb_count_n"] + err ))
+    mask = df["map_count_mean_n"] >= (df["map_glb_count_n"] - err)
+    mask = mask & (df["map_count_mean_n"] <= (df["map_glb_count_n"] + err))
     df["convergence_mask"] = mask
 
-    ret  = []
+    ret = []
     for g, _df in df.groupby(["sim"]):
-        ret.append([g, _df["convergence_mask"].idxmax()[-1]-time_slice.start , map.loc[pd.IndexSlice[g, :, "map_glb_count"],].max()])
-    
-    return pd.DataFrame.from_records(ret, columns=["sim", "convergence_time", "ped_count"])
+        ret.append(
+            [
+                g,
+                _df["convergence_mask"].idxmax()[-1] - time_slice.start,
+                map.loc[
+                    pd.IndexSlice[g, :, "map_glb_count"],
+                ].max(),
+            ]
+        )
+
+    return pd.DataFrame.from_records(
+        ret, columns=["sim", "convergence_time", "ped_count"]
+    )
+
 
 def main(study: SuqcRun):
 
     map = read_data(study).sort_index()
 
-    # get convergence time 
-    df_convergence = get_convergence_time(map, time_slice = slice(41,None), err=0.05)
+    # get convergence time
+    df_convergence = get_convergence_time(map, time_slice=slice(41, None), err=0.05)
     df_convergence.to_csv(os.path.join(study.base_path, "convergence_time.csv"))
 
     # create figures
-    fig, axes = plt.subplots(3,1,figsize=(16,3*9))
-
+    fig, axes = plt.subplots(3, 1, figsize=(16, 3 * 9))
 
     for ax, lbl_filter in zip(axes, ["ped", "415", "207"]):
         ax.set_ylim(0.0, 1.1)
@@ -171,50 +210,78 @@ def main(study: SuqcRun):
 
         _first_run = map.index.get_level_values(0)[0]
         _glb = map.loc[pd.IndexSlice[_first_run, :, "map_glb_count_n"]]
-        ax.plot(_glb.index.get_level_values("simtime"), _glb, label="ground_truth", color="black")
+        ax.plot(
+            _glb.index.get_level_values("simtime"),
+            _glb,
+            label="ground_truth",
+            color="black",
+        )
         for sim in map.index.get_level_values("sim").unique():
             if lbl_filter in sim:
-                _df = map.loc[pd.IndexSlice[sim,:, "map_count_mean_n"]]
-                ax.plot( _df.index, _df, label=sim)
+                _df = map.loc[pd.IndexSlice[sim, :, "map_count_mean_n"]]
+                ax.plot(_df.index, _df, label=sim)
         ax.legend(loc="center right")
 
-
     fig.tight_layout()
-    fig.savefig(os.path.join(study.base_path, 'normalized_pedestrian_count.pdf'))
+    fig.savefig(os.path.join(study.base_path, "normalized_pedestrian_count.pdf"))
 
 
 def plot_positions(study: SuqcRun, run_map: dict):
 
-    args = [ dict(study=study, run_dict=val)  for val in run_map.values()]
+    args = [dict(study=study, run_dict=val) for val in run_map.values()]
     position_data = run_kwargs_map(
         func=OppAnalysis.merge_position,
         kwargs_iter=args,
-        append_args=True,        
+        append_args=True,
     )
 
     figures = []
     for pos_df, kwargs in position_data:
         rep_list = kwargs["run_dict"]["rep"]
         lbl = kwargs["run_dict"]["lbl"]
-        sim = study.get_sim(rep_list[0]) # for offset only. Is equal for all rep's
+        sim = study.get_sim(rep_list[0])  # for offset only. Is equal for all rep's
         _, bound = sim.sql.get_sim_offset_and_bound()
         bound_poly = sim.sql.get_bound_polygon().exterior.xy
-        fig, axes = plt.subplots(3, 4, figsize=(16, 9), sharex='all', sharey='all')
+        fig, axes = plt.subplots(3, 4, figsize=(16, 9), sharex="all", sharey="all")
         fig.suptitle(f"Position for scenario {lbl} with N=10 seeds.", fontsize=16)
         axes = list(itertools.chain(*axes))
         for i in range(len(rep_list), len(axes)):
             fig.delaxes(axes[i])
         for idx, run_id in enumerate(rep_list):
-            ax:plt.Axes = axes[idx]
+            ax: plt.Axes = axes[idx]
             ax.set_title(f"Run N={idx}")
-            ax.set_aspect('equal', adjustable='box')
-            ax.set_xlim(-10, bound[0]+10)
-            ax.set_ylim(-10, bound[1]+10)
-            ax.plot("x", "y", label="nodes stationary for total simulation", data=pos_df.loc[run_id, :, :, True],  markersize=3, marker='o', linestyle='none', color="red")
-            ax.plot("x", "y", label="nodes removed after t=40s", data=pos_df.loc[run_id, :, :, False], markersize=3, marker='o',linestyle='none',  color="blue")
+            ax.set_aspect("equal", adjustable="box")
+            ax.set_xlim(-10, bound[0] + 10)
+            ax.set_ylim(-10, bound[1] + 10)
+            ax.plot(
+                "x",
+                "y",
+                label="nodes stationary for total simulation",
+                data=pos_df.loc[run_id, :, :, True],
+                markersize=3,
+                marker="o",
+                linestyle="none",
+                color="red",
+            )
+            ax.plot(
+                "x",
+                "y",
+                label="nodes removed after t=40s",
+                data=pos_df.loc[run_id, :, :, False],
+                markersize=3,
+                marker="o",
+                linestyle="none",
+                color="blue",
+            )
             ax.plot(bound_poly[0], bound_poly[1], color="red")
-        fig.legend(*axes[0].get_legend_handles_labels(), loc="lower center", ncol=2, bbox_to_anchor=(.5,0), bbox_transform=fig.transFigure)
-        fig.tight_layout(rect=(0, .05, 1., 1.))
+        fig.legend(
+            *axes[0].get_legend_handles_labels(),
+            loc="lower center",
+            ncol=2,
+            bbox_to_anchor=(0.5, 0),
+            bbox_transform=fig.transFigure,
+        )
+        fig.tight_layout(rect=(0, 0.05, 1.0, 1.0))
         figures.append(fig)
         print(f"done with {lbl}")
 
