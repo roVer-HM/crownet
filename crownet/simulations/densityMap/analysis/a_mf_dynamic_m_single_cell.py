942bed3dacb89bbd3f80d030b82686c08049207a
Stefan Schuhbäck <stefan.schuhbaeck@hm.edu>
2022-08-05 15:02:17 +0200

    S3 refactor: allow RunMap with Simulations from multiple Studies

--- crownet/simulations/densityMap/analysis/a_mf_dynamic_m_single_cell.py
+++ crownet/simulations/densityMap/analysis/a_mf_dynamic_m_single_cell.py
@@ -7,9 +7,9 @@ from typing import Any, Callable, Dict, List, Tuple
 from roveranalyzer.analysis.common import (
     RunContext,
     Simulation,
+    SimulationGroup,
     SuqcStudy,
     RunMap,
-    Parameter_Variation,
 )
 from roveranalyzer.analysis.omnetpp import OppAnalysis
 from roveranalyzer.utils.parallel import run_kwargs_map
@@ -23,107 +23,68 @@ from matplotlib.backends.backend_pdf import PdfPages
 from omnetinireader.config_parser import OppConfigFileBase, ObjectValue
 
 
-def lbl_f_alpha_dist(run_id: int | Tuple[int, int], study: SuqcStudy) -> str:
-    sim: Simulation = study.get_sim(run_id)
+def lbl_f_alpha_dist(sim: Simulation) -> str:
     cfg: OppConfigFileBase = sim.run_context.oppini
     mapCfg: ObjectValue = cfg.get("*.misc[*].app[1].app.mapCfg")
     return f"({mapCfg['alpha']},{mapCfg['stepDist']})"
 
 
-def get_run_map(
-    study: SuqcStudy, rep_count=10, map_filter: Callable[[Any], True] = lambda x: True
-) -> RunMap:
-    """Create RunMap labels based on alpha / cut off distance
-
-    Args:
-        study (SuqcRun): _description_
-        rep_count (int, optional): Number of seeds used. Defaults to 10.
-        map_filter (_type_, optional): _description_. Defaults to lambda x:True.
-
-    Returns:
-        RunMap:  A dictionary with [str, ParameterVariation]
-    """
-
-    @dataclass(frozen=True)
-    class _lbl_class:
-        alpha: float = -1.0
-        dist: float = -1.0
-        iat: float = -1.0
-
-        @classmethod
-        def from_sim_v(cls, sim: Simulation, *args: Any, **kwds: Any) -> _lbl_class:
-            cfg: OppConfigFileBase = sim.run_context.oppini
-            mapCfg: ObjectValue = cfg.get("*.pNode[*].app[1].app.mapCfg")
-            scenario = cfg.get("**.vadereScenarioPath")
-            r = re.compile(r".*_(\d+).*")
-            if match := r.match(scenario):
-                iat = match.groups()[0]
-            else:
-                iat = "??"
-            return str(cls(alpha=mapCfg["alpha"], dist=mapCfg["stepDist"], iat=iat))
-
-        @classmethod
-        def from_sim_bm(cls, sim: Simulation) -> _lbl_class:
-            cfg: OppConfigFileBase = sim.run_context.oppini
-            mapCfg: ObjectValue = cfg.get("*.misc[*].app[1].app.mapCfg")
-            scenario = cfg.get("*.bonnMotionServer.traceFile")
-            r = re.compile(r".*exp_(\d+).*")
-            if match := r.match(scenario):
-                iat = match.groups()[0]
-            else:
-                iat = "??"
-            return str(cls(alpha=mapCfg["alpha"], dist=mapCfg["stepDist"], iat=iat))
-
-        def __call__(self, *args: Any, **kwds: Any) -> Any:
-            return self.__str__()
-
-        def __str__(self) -> str:
-            return self.__repr__()
-
-        def __repr__(self) -> str:
-            return f"{self.alpha}_{self.dist}_{self.iat}"
-
-    run_map = study.create_run_map(
-        rep=rep_count, lbl_f=_lbl_class.from_sim_bm, id_filter=map_filter
-    )
-    return run_map
+def from_sim_v(sim: Simulation) -> str:
+    cfg: OppConfigFileBase = sim.run_context.oppini
+    mapCfg: ObjectValue = cfg.get("*.pNode[*].app[1].app.mapCfg")
+    scenario = cfg.get("**.vadereScenarioPath")
+    r = re.compile(r".*_(\d+).*")
+    if match := r.match(scenario):
+        iat = match.groups()[0]
+    else:
+        iat = "??"
+    return f'{mapCfg["alpha"]}_{mapCfg["stepDist"]}_{iat}'
 
 
-def collect_count_error_for_parameter_variation(
-    study: SuqcStudy, par_var: Parameter_Variation
-):
+def from_sim_bm(sim: Simulation) -> str:
+    cfg: OppConfigFileBase = sim.run_context.oppini
+    mapCfg: ObjectValue = cfg.get("*.misc[*].app[1].app.mapCfg")
+    scenario = cfg.get("*.bonnMotionServer.traceFile")
+    r = re.compile(r".*exp_(\d+).*")
+    if match := r.match(scenario):
+        iat = match.groups()[0]
+    else:
+        iat = "??"
+    return f'{mapCfg["alpha"]}_{mapCfg["stepDist"]}_{iat}'
+
+
+def collect_count_error_for_simulation_group(sim_group: SimulationGroup):
     df = []
-    for idx, rep in enumerate(par_var.reps):
-        sim: Simulation = study.get_sim(rep)
+    for idx, sim in enumerate(sim_group.simulations):
         _df = sim.get_dcdMap().map_count_measure().loc[:, ["map_mean_err"]]
-        _df.columns = [f"{idx}-{par_var['lbl']}"]
+        _df.columns = [f"{idx}-{sim_group.group_name}"]
         df.append(_df)
 
     df = pd.concat(df, axis=1, verify_integrity=True)
     return df
 
 
-def plot_count_error(study: SuqcStudy, run_map: RunMap, figure_name: str):
+def plot_count_error(run_map: RunMap, figure_name: str):
 
     data: List[(pd.DataFrame, dict)] = run_kwargs_map(
-        collect_count_error_for_parameter_variation,
-        [dict(study=study, par_var=v) for v in run_map.get_parameter_variations()],
+        collect_count_error_for_simulation_group,
+        [dict(sim_group=v) for v in run_map.get_simulation_group()],
         pool_size=10,
         append_args=True,
     )
 
-    with PdfPages(study.path(figure_name)) as pdf:
+    with PdfPages(run_map.path(figure_name)) as pdf:
         fig, axes = plt.subplots(4, 4, figsize=(16, 9), sharex="all", sharey="all")
         fig.suptitle("Count Error for alpha_dist_iat")
         axes = list(chain(*axes))
         for idx, d in enumerate(data):
             df = d[0]
-            par_var: Parameter_Variation = d[1]["par_var"]
-            if "1_999" in par_var.label:
+            sim_group: SimulationGroup = d[1]["sim_group"]
+            if "1_999" in sim_group.label:
                 # ignore last one
                 continue
             ax: plt.Axes = axes[idx]
-            ax.set_title(f"{par_var.label}")
+            ax.set_title(f"{sim_group.label}")
             ax.axhline(y=0, color="black")
             for n, col in enumerate(df.columns):
                 ax.plot(df.index, df[col], label=f"run_{n}", linewidth=1)
@@ -141,12 +102,10 @@ def plot_count_error(study: SuqcStudy, run_map: RunMap, figure_name: str):
         pdf.savefig(fig)
 
 
-def plot_mse_cell_over_time(
-    study: SuqcStudy, run_map: RunMap, data: pd.DataFrame, figure_name: str
-):
+def plot_mse_cell_over_time(run_map: RunMap, data: pd.DataFrame, figure_name: str):
     """Plot cell mean squared error over time for each variation. One line per seed."""
 
-    with PdfPages(study.path(figure_name)) as pdf:
+    with PdfPages(run_map.path(figure_name)) as pdf:
         fig, axes = plt.subplots(4, 4, figsize=(16, 9), sharex="all", sharey="all")
         fig.suptitle("Mean Squared Cell Error for alpha_dist_iat")
         axes = list(chain(*axes))
@@ -176,24 +135,24 @@ def plot_mse_cell_over_time(
 
 def _get_alpha_dist(
     run_map: RunMap,
-) -> Tuple[Dict[str, Parameter_Variation], Dict[str, Parameter_Variation]]:
+) -> Tuple[Dict[str, List[SimulationGroup]], Dict[str, List[SimulationGroup]]]:
     """Sort parameter variations based on alpha and dist. Each variation is part of both Dict[str, ...]
     returned from this function.
 
     Returns:
-        Tuple[Dict[str, Parameter_Variation], Dict[str, Parameter_Variation]]: _description_
+        Tuple[Dict[str, List[SimulationGroup] Dict[str, List[SimulationGroup]]]: _description_
     """
     alpha_ = {}
     dist_ = {}
-    for par_var in run_map.get_parameter_variations():
-        a = float(par_var.label.split("_")[0])
+    for sim_group in run_map.get_simulation_group():
+        a = float(sim_group.group_name.split("_")[0])
         _data = alpha_.get(a, [])
-        _data.append(par_var)
+        _data.append(sim_group)
         alpha_[a] = _data
 
-        d = float(par_var.label.split("_")[1])
+        d = float(sim_group.group_name.split("_")[1])
         _data = dist_.get(d, [])
-        _data.append(par_var)
+        _data.append(sim_group)
         dist_[d] = _data
     return alpha_, dist_
 
@@ -205,7 +164,6 @@ def _add_sorted_legends(ax: plt.Axes):
 
 
 def plot_mse_all(
-    study: SuqcStudy,
     data: pd.DataFrame,
     run_map: RunMap,
     styles: StyleMap,
@@ -218,14 +176,14 @@ def plot_mse_all(
     ax_a: plt.Axes = ax[0]
     ax_b: plt.Axes = ax[1]
 
-    for alpha, par_vars in alpha_.items():
-        for par_var in par_vars:
-            reps = par_var.reps
+    for alpha, groups in alpha_.items():
+        for sim_group in groups:
+            reps = sim_group.reps
             ax_a.plot(
                 alpha * np.ones(len(reps)),
-                data.loc[reps],
-                label=par_var.label,
-                **styles.get_style(par_var.label),
+                data.loc[reps, ["cell_mse"]],
+                label=sim_group.group_name,
+                **styles.get_style(sim_group.group_name),
             )
     ax_a.set_xlim(0.45, 1.0)
     ax_a.set_title("Cell MSE over alpha")
@@ -233,24 +191,24 @@ def plot_mse_all(
     ax_a.set_xlabel("Alpha")
     _add_sorted_legends(ax_a)
 
-    for dist, par_vars in dist_.items():
-        for par_var in par_vars:
-            reps = par_var.reps
+    for dist, groups in dist_.items():
+        for sim_group in groups:
+            reps = sim_group.reps
             ax_b.plot(
                 dist * np.ones(len(reps)),
-                data.loc[reps],
-                label=par_var.label,
-                **styles.get_style(par_var.label),
+                data.loc[reps, ["cell_mse"]],
+                label=sim_group.group_name,
+                **styles.get_style(sim_group.group_name),
             )
     ax_b.set_title("Cell MSE over distance")
     ax_b.set_xlabel("Distance threshold")
     _add_sorted_legends(ax_b)
 
-    fig.savefig(study.path(figure_name))
+    fig.savefig(run_map.path(figure_name))
 
 
 def plot_mse_for_seed(
-    study: SuqcStudy, data: pd.DataFrame, run_map: RunMap, styles: StyleMap, figure_name
+    data: pd.DataFrame, run_map: RunMap, styles: StyleMap, figure_name
 ):
     """Plot cell mse for each axis (alpha, cut off distance) and each seed. Add comparison
     line for youngest measurement first (i.e. alpha=1.0)
@@ -258,9 +216,9 @@ def plot_mse_for_seed(
 
     alpha_, dist_ = _get_alpha_dist(run_map)
 
-    with PdfPages(study.path(figure_name)) as pdf:
+    with PdfPages(run_map.path(figure_name)) as pdf:
         for _seed in range(10):
-            ymf_err = data.loc[run_map["1_999_25"]["rep"][_seed]]
+            ymf_err = data.loc[run_map["1_999_25"][_seed].global_id()]
             fig, ax = plt.subplots(1, 2, figsize=(16, 9), sharey="all")
             fig.suptitle(f"Seed {_seed}")
             ax_a: plt.Axes = ax[0]
@@ -268,14 +226,14 @@ def plot_mse_for_seed(
 
             # ax_a.set_ylim(0.0015, 0.0025)
             ax_a.set_xlim(0.45, 1.0)
-            for a, runs in alpha_.items():
-                for r in runs:
-                    reps = r["rep"]
+            for a, groups in alpha_.items():
+                for sim_group in groups:
+                    reps = sim_group.reps
                     ax_a.plot(
                         a,
                         data.loc[reps[_seed]],
-                        label=r["lbl"],
-                        **styles.get_style(r["lbl"]),
+                        label=sim_group.group_name,
+                        **styles.get_style(sim_group.group_name),
                     )
             ax_a.set_title("Cell MSE over alpha")
             ax_a.set_ylabel("Cell MSE")
@@ -283,14 +241,14 @@ def plot_mse_for_seed(
             _add_sorted_legends(ax_a)
             ax_a.hlines(ymf_err, 0.45, 1.0, colors="red")
 
-            for a, runs in dist_.items():
-                for r in runs:
-                    reps = r["rep"]
+            for a, groups in dist_.items():
+                for sim_group in groups:
+                    reps = sim_group.reps
                     ax_b.plot(
                         a,
                         data.loc[reps[_seed]],
-                        label=r["lbl"],
-                        **styles.get_style(r["lbl"]),
+                        label=sim_group.group_name,
+                        **styles.get_style(sim_group.group_name),
                     )
 
             # ax_b.set_ylim(0.0015, 0.0025)
@@ -302,7 +260,6 @@ def plot_mse_for_seed(
 
 
 def plot_mse_yDist_to_ymf(
-    study: SuqcStudy,
     data: pd.DataFrame,
     run_map: RunMap,
     figure_name: str,
@@ -311,40 +268,39 @@ def plot_mse_yDist_to_ymf(
     For each parameter variation one bar chart with each bar is one seed (run_id).
 
     Args:
-        study (SuqcRun): _description_
         data (pd.DataFrame): _description_
         run_map (dict): _description_
         figure_name (str): _description_
     """
 
-    with PdfPages(study.path(figure_name)) as pdf:
+    with PdfPages(run_map.path(figure_name)) as pdf:
         fig, axes = plt.subplots(4, 4, figsize=(16, 9), sharey="all")
         fig.suptitle("Cell MSE difference between ymfDist and ymf")
         axes = list(chain(*axes))
 
         ymin = data["err_to_ymf"].min()
         ymax = data["err_to_ymf"].max()
-        for idx, run_dict in enumerate(run_map.items()):
+        for idx, item in enumerate(run_map.items()):
             if idx == 16:
                 continue
+            group_name = item[0]
+            sim_group: SimulationGroup = item[1]
+            seed_num = len(sim_group)
             ax: plt.Axes = axes[idx]
-            ax.hlines(0, -1, 10, colors="gray")
-            ax.set_xticks(np.arange(10), labels=np.arange(10))
+            ax.hlines(0, -1, seed_num, colors="gray")
+            ax.set_xticks(np.arange(seed_num), labels=np.arange(seed_num))
             ax.set_ylim(ymin, ymax)
-            rep = run_dict[1]["rep"]
-            lbl = run_dict[0]
-            err = data.loc[rep, ["err_to_ymf"]]
-            ax.bar(np.arange(10), err.iloc[:, 0].to_numpy(), 0.35)
+            err = data.loc[sim_group.reps, ["err_to_ymf"]]
+            ax.bar(np.arange(len(sim_group)), err.iloc[:, 0].to_numpy(), 0.35)
             ax.set_ylabel("Cell MSE diff ymfDist-ymf")
             ax.set_xlabel("Seeds / Runs")
-            ax.set_title(lbl)
+            ax.set_title(group_name)
         fig.tight_layout()
         pdf.savefig(fig)
     print("done")
 
 
 def plot_mse_yDist_to_ymf_box_plot(
-    study: SuqcStudy,
     run_map: RunMap,
     data: pd.DataFrame,
     figure_name: str,
@@ -353,13 +309,10 @@ def plot_mse_yDist_to_ymf_box_plot(
     # create mean cell mse for each run_id over time.
     # data = data.groupby("run_id").mean()
     # add label of the form '({alpha}, {dist}) to data
-    lbl_f = partial(lbl_f_alpha_dist, study=study)
-    lbl_df = run_map.id_to_label_series(lbl_f=lbl_f, enumerate_run=True)
     data = (
-        data.join(lbl_df)
-        .reset_index()
+        data.reset_index()
+        .drop(columns=["seed", "run_id", "run_id_ymf", "cell_mse", "ymf_cell_mse"])
         .set_index(["run_index", "label"])
-        .drop(columns=["run_id", "cell_mse"])
         .unstack(["label"])
     )
     sorted_index = data.mean().sort_values().index
@@ -377,43 +330,56 @@ def plot_mse_yDist_to_ymf_box_plot(
     ax.axhline(y=0, color="red")
     ax.axvline(x=np.argmax(data.columns == "(1,999)") + 1, color="red")
 
-    fig.savefig(study.path(figure_name))
+    fig.savefig(run_map.path(figure_name))
 
 
 def plot_all(
-    study: SuqcStudy,
     run_map: RunMap,
     styles: StyleMap,
     data: pd.DataFrame,
     data_mean_by_run_id: pd.DataFrame,
 ):
 
-    plot_mse_all(
-        study, data_mean_by_run_id, run_map, styles, figure_name="cell_mse.pdf"
-    )
+    plot_mse_all(data_mean_by_run_id, run_map, styles, figure_name="cell_mse.pdf")
     plot_mse_for_seed(
-        study,
         data_mean_by_run_id["cell_mse"],
         run_map,
         styles,
         figure_name="cell_mse_for_seed.pdf",
     )
-    plot_mse_yDist_to_ymf(study, data_mean_by_run_id, run_map, "mse_diff.pdf")
-    # plot_mse_cell_over_time(study, run_map, data, "cell_mse_over_time.pdf")
-    # plot_count_error(study, run_map, "count_error_over_time.pdf")
+    plot_mse_yDist_to_ymf(data_mean_by_run_id, run_map, "mse_diff.pdf")
+    # plot_mse_cell_over_time(run_map, data, "cell_mse_over_time.pdf")
+    # plot_count_error(run_map, "count_error_over_time.pdf")
+    plot_mse_yDist_to_ymf_box_plot(
+        run_map, data_mean_by_run_id, "cell_mse_box_plot.pdf"
+    )
 
 
-def _get_mse_data(study: SuqcStudy, run_map: RunMap):
+def _get_mse_data(run_map: RunMap):
 
     data = OppAnalysis.get_mse_cell_data_for_study(
-        study, run_map, hdf_path="cell_mse.h5", cell_count=84 * 79, pool_size=20
+        run_map, hdf_path="cell_mse.h5", cell_count=84 * 79, pool_size=20
     )
-    data_mean_by_run_id = data.groupby(by="run_id").mean()
+    data_mean_by_run_id = data.groupby(by="run_id").mean().to_frame()
+    seeds = run_map.id_to_label_series(lbl_f=lbl_f_alpha_dist, enumerate_run=True)
+    data_mean_by_run_id = data_mean_by_run_id.join(seeds)
 
-    # repeat  ymf error to calculate mse_diff for each run
-    ymf_err = np.tile(data_mean_by_run_id.loc[run_map["1_999_25"].reps], reps=17)
-    data_mean_by_run_id = data_mean_by_run_id.to_frame()
-    data_mean_by_run_id["err_to_ymf"] = data_mean_by_run_id["cell_mse"] - ymf_err
+    # add ymf error column to all calculate difference
+    ymf_group = run_map["1_999_25"]
+    ymf_err = data_mean_by_run_id.loc[
+        ymf_group.reps, ["cell_mse", "seed"]
+    ].reset_index()
+    ymf_err.columns = ["run_id_ymf", "ymf_cell_mse", "seed"]
+    data_mean_by_run_id = (
+        data_mean_by_run_id.reset_index()
+        .merge(ymf_err, on="seed")
+        .set_index("run_id")
+        .sort_index()
+    )
+
+    data_mean_by_run_id["err_to_ymf"] = (
+        data_mean_by_run_id["cell_mse"] - data_mean_by_run_id["ymf_cell_mse"]
+    )
     return data, data_mean_by_run_id
 
 
@@ -426,15 +392,28 @@ def main():
     # study = SuqcRun("/mnt/data1tb/results/mf_dynamic_m_single_cell_iat25_1/")
     # study = SuqcRun("/mnt/data1tb/results/mf_dynamic_m_single_cell_iat25_2/")
     # study = SuqcRun("/mnt/data1tb/results/mf_dynamic_m_single_cell_iat25_3/")
-    study = SuqcStudy("/mnt/data1tb/results/mf_dynamic_m_single_cell_iat25_4/")
-    run_map: RunMap = get_run_map(study, map_filter=id_filter)
-    styles = StyleMap(markersize=3, marker="o", linestyle="none")
-    data, data_mean_by_run_id = _get_mse_data(study, run_map)
+    study1 = SuqcStudy("/mnt/data1tb/results/mf_dynamic_m_single_cell_iat25_4/")
+    study2 = SuqcStudy("/mnt/data1tb/results/mf_dynamic_m_single_cell_iat25_test/")
 
-    plot_mse_yDist_to_ymf_box_plot(
-        study, run_map, data_mean_by_run_id, "cell_mse_box_plot.pdf"
+    run_map: RunMap = RunMap(output_dir="/mnt/data1tb/results/mf_dyn_20_seeds")
+
+    run_map = study1.update_run_map(
+        run_map, sim_per_group=10, id_offset=0, lbl_f=from_sim_bm, allow_new_groups=True
     )
-    # plot_all(study, run_map, styles, data, data_mean_by_run_id)
+    run_map = study2.update_run_map(
+        run_map,
+        sim_per_group=10,
+        id_offset=len(study1),
+        lbl_f=from_sim_bm,
+        allow_new_groups=False,
+    )
+    styles = StyleMap(markersize=3, marker="o", linestyle="none")
+    data, data_mean_by_run_id = _get_mse_data(run_map)
+
+    # plot_mse_yDist_to_ymf_box_plot(
+    #     study, run_map, data_mean_by_run_id, "cell_mse_box_plot.pdf"
+    # )
+    plot_all(run_map, styles, data, data_mean_by_run_id)
 
 
 if __name__ == "__main__":
