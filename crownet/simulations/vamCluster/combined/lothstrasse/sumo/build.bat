b35eddc492b665f869cb2195d5920af453a53fb3
Matthias Rupp <mrupp@hm.edu>
2022-11-22 10:12:52 +0100

    Add generated scenarios

--- crownet/simulations/vamCluster/combined/lothstrasse/sumo/build.bat
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python "$SUMO_HOME/tools/randomTrips.py" -n osm.net.xml --fringe-factor 5 -p 136.694188 -o osm.passenger.trips.xml -e 3600 --vehicle-class passenger --vclass passenger --prefix veh --min-distance 300 --trip-attributes "departLane=\"best\"" --fringe-start-attributes "departSpeed=\"max\"" --allow-fringe.min-length 1000 --lanes --validate
