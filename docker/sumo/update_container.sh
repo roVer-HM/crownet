d8bc6bc76f19a9aab430c42822a6c64cc4ba0a64
Christina Maria Mayr <mayr9@hm.edu>
2023-04-21 10:25:36 +0000

    Docker image: refactor + add deploy to CI

--- docker/sumo/update_container.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-CONTAINER='sumo'
-VERSION_TAG='v1_15_0'
-# VERSION_TAG='latest'
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-$DIR/../../scripts/upd_container.sh $CONTAINER $VERSION_TAG
-
