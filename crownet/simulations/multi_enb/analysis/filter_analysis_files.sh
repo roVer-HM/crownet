daa04035efc493075468cb3c3323bd8cb5250253
stefan Schuhbaeck <schuhbaeck@cmp-05.cm.in.tum.de>
2024-03-21 15:53:13 +0100

    update file filter for map_size_global.csv

--- crownet/simulations/multi_enb/analysis/filter_analysis_files.sh
+++ crownet/simulations/multi_enb/analysis/filter_analysis_files.sh
@@ -7,16 +7,33 @@ enb_rb.h5|\
 node_tx_data.h5|\
 node_rx_data.h5|\
 entropy_map_size.csv|\
+entropy_map_size_global.csv|\
+density_map_size.csv|\
+density_map_size_global.csv|\
 entropy_map_measurements_age_over_distance.h5|\
-entropy_map_size_and_age_over_distance.h5"
+entropy_map_size_and_age_over_distance.h5|\
+DENSITY.cfg|\
+ENTROPY.cfg"
 
-find $1 -path "*Sample*$2*" -type f -exec ls -l {} \; | # create file list
+GREP_STR="entropy_map_size.csv|\
+entropy_map_size_global.csv|\
+density_map_size.csv|\
+density_map_size_global.csv"
+
+_tmp=$(mktemp --suffix "seed_$2")
+
+
+find $1 -path "*Sample*$2_0*" -type f -exec ls -l {} \; | # create file list
 	tr -s ' ' | # collapse multiple strings
 	awk -F ' '  '{print $9 " "  $5;}' | # select name and size columns 
-	grep -E "${GREP_STR}"  > "analysis_file_list_seed_$2.txt"  # select copied files and save to file 
+	grep -E "${GREP_STR}"  > $_tmp  # select copied files and save to file 
 
-cat "analysis_file_list_seed_$2.txt" | awk '{sum+=$2}END{print NR" files with total size of " sum/1000000000" GB";}'
 
-cat "analysis_file_list_seed_$2.txt" | awk '{print $1}' > "tmp_analysis_file_list_seed_$2.txt"
+cat $_tmp | awk '{print $1}'
 
-mv  "tmp_analysis_file_list_seed_$2.txt" "analysis_file_list_seed_$2.txt"
+if [ -t 1 ] ; then
+    cat $_tmp | awk '{sum+=$2}END{print NR" files with total size of " sum/1000000000" GB";}'
+fi
+
+rm $_tmp
+
