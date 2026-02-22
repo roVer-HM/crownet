0fe10d831da743c22728f6e5c01d48bdd7113c3c
Jixuan Wang <jixuan.wang1@hm.edu>
2026-02-22 17:41:45 +0100

    fix adaptivemap runscript to clean up old simulation output

--- crownet/simulations/adaptiveMap/run_script.py
+++ crownet/simulations/adaptiveMap/run_script.py
@@ -83,6 +83,31 @@ class SimulationRun(BaseSimulationRunner):
     def __init__(self, working_dir, args=None):
         super().__init__(working_dir, args)
 
+    def run(self):
+        import glob
+        import os
+        import shutil
+        res_dir = self.ns["opp_args"].get_value("--result-dir")
+        if not res_dir:
+            res_dir = self.ns.get("result_dir")
+            if not res_dir:
+                res_dir = self.result_dir()
+        if os.path.exists(res_dir):
+            for f in glob.glob(os.path.join(res_dir, "*.db")) + glob.glob(os.path.join(res_dir, "*.csv")):
+                try:
+                    os.remove(f)
+                except Exception:
+                    pass
+        super().run()
+        
+        sim_res_dir = self.result_dir()
+        if res_dir and res_dir != sim_res_dir and os.path.exists(res_dir) and os.path.exists(sim_res_dir):
+            for f in glob.glob(os.path.join(res_dir, "*.db")) + glob.glob(os.path.join(res_dir, "*.csv")):
+                try:
+                    shutil.move(f, os.path.join(sim_res_dir, os.path.basename(f)))
+                except Exception:
+                    pass
+
     def get_cfg(self):
         return get_density_cfg(self.result_base_dir())
 
